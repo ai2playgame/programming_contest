874cde445aa401d7adf42ebe269662badf22035d
ai2playgame <detective9684@gmail.com>
2022-09-18 18:39:24 +0900

    ABC269 D

--- atcoder/ABC269/D.cpp
+++ atcoder/ABC269/D.cpp
@@ -15,20 +15,44 @@ const int MOD = 1000000007;
 
 int main()
 {
-    /* code */
     const int dx[6] = {-1, -1, 0, 0, 1, 1};
     const int dy[6] = {-1, 0, -1, 1, 0, 1};
 
     int n;
     cin >> n;
-    vector<pair<int, int>> u(n);
+    using P = pair<int, int>;
+    set<P> u;
     REP(i, n)
     {
         int x, y;
         cin >> x >> y;
-        u[i] = make_pair(x, y);
+        u.insert(make_pair(x, y));
     }
 
+    ll ans = 0;
+    while (u.size() > 0)
+    {
+        P p = *u.begin();
+        u.erase(p);
+        ans++;
+        stack<P> stack;
+        stack.push(p);
+        while (stack.size() > 0)
+        {
+            P startP = stack.top();
+            stack.pop();
 
+            for (int d = 0; d < 6; d++)
+            {
+                P newP = make_pair(startP.first + dx[d], startP.second + dy[d]);
+                if (u.find(newP) == u.end())
+                    continue;
+                stack.push(newP);
+                u.erase(newP);
+            }
+        }
+    }
+
+    PRINT(ans);
     return 0;
 }
